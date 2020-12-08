28e042926924d29bb33d9cc1a7dd0e15d230e607
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-07 20:01:39 -0600

    added class constructor for App

--- src/components/app.jsx
+++ src/components/app.jsx
@@ -2,7 +2,18 @@ import React, { Component } from "react";
 import TitleBar from "./TitleBar/titleBar"
 
 class App extends Component {
-    state = { }
+    constructor(props) {
+        super(props);
+        this.books = [
+            {title: "Ready Player One", author: "Ernest Cline"},
+            {title: "All the Light We Cannot See", author: "Anthony Doerr"},
+            {title: "The First and Last Freedom", author: "Jiddu Krishnamurti"}
+        ]
+    }
+    state = {
+        bookNumber:0
+    }
+
     render(){
         return (
             <div className="container-fluid">
