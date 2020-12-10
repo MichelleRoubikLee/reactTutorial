ceeda3438000a32da056776ee5ed7d36689c8678
MichelleRoubikLee <michelleroubiklee@gmail.com>
2020-12-10 16:04:53 -0600

    book creator added for adding books to list

---
+++ src/components/Footer/footer.jsx
@@ -0,0 +1,16 @@
+import React from "react";
+import "./footer.css";
+
+function Footer(props) {
+    return (
+        <div className="row row-spacer">
+            <div className="col-md-12">
+                <div className="titlebar-nav">
+                    <h1>Michelle Library Footer</h1>
+                </div>
+            </div>
+        </div>
+    );
+}
+
+export default Footer;
